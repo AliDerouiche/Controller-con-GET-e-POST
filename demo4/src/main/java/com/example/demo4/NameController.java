ae188254e71a33b84a7224eb4c08d36b07334987
Bisco <alidero1605@gmail.com>
2024-04-29 15:56:41 +0200

    Controller con GET e POST

--- demo4/src/main/java/com/example/demo4/NameController.java
+++ demo4/src/main/java/com/example/demo4/NameController.java
@@ -1,20 +1,25 @@
 package com.example.demo4;
-
+import io.swagger.v3.oas.annotations.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/name")
 public class NameController {
 
     @GetMapping("/obtain")
-    public String getName(@RequestParam String name) {
+    @Operation(summary = ("get the written name"))
+    public String getName(@Parameter(description = "insert the name to print")@RequestParam String name) {
         return name;
     }
 
     @PostMapping("/reverse")
-    public String postName(@RequestParam String name) {
+    @Operation(summary = ("revert written name"))
+    public String postName(@Parameter(description = "insert the name to revert")@RequestParam String name) {
         StringBuilder sb = new StringBuilder(name);
         sb.reverse();
         return sb.toString();
     }
 }
+
